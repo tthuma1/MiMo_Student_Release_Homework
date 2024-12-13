905d2c5520248c658d8c573a9e90bbd10fad4231
Tim Thuma <thumatim@gmail.com>
2024-12-13 21:31:51 +0100

    added subc

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 0xffff
-    li r1, 2
-    addc r2, r1, r0, 10
+main: li r0, 5
+    li r1, 5
+    jeq r1, r0, 10
         
