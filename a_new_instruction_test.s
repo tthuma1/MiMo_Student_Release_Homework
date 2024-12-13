3b02237d7b895b96d355b992a3702a608595de56
Tim Thuma <thumatim@gmail.com>
2024-12-13 23:01:46 +0100

    added jlt, jge, jeqz

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 7
-    li r1, 6
-    jle r0, r1, 10
+main: li r0, 6
+    li r1, 7
+    jlt r0, r1, 10
         
