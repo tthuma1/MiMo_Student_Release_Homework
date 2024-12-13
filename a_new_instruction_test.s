76c3834ff168530b80357a1debcb6806f9bd2f4b
Tim Thuma <thumatim@gmail.com>
2024-12-13 21:25:26 +0100

    added addc

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,5 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main:	li	  r1, 2				# r0 is the running sum
-        muli r0, r1, 5
+main: li r0, 0xffff
+    li r1, 2
+    addc r2, r1, r0, 10
+        
