45088b1cd817731998db464cbf676dfec89e4f20
Tim Thuma <thumatim@gmail.com>
2024-12-13 23:08:28 +0100

    added branches

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,6 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 6
-    li r1, 7
-    jlt r0, r1, 10
-        
+main: li r1, 10
+    li r2, 10
+    bne r1, r2, 10
