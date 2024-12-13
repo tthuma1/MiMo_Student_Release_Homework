28989b44c8583e0faf47c117250eadf5ded65cbe
Tim Thuma <thumatim@gmail.com>
2024-12-13 22:54:42 +0100

    added jle

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 6
-    li r1, 5
-    jne r1, r0, 10
+main: li r0, 7
+    li r1, 6
+    jle r0, r1, 10
         
