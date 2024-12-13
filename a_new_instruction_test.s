27058623364fab03534e4e019d1eb35a6188aef1
Tim Thuma <thumatim@gmail.com>
2024-12-13 22:43:23 +0100

    added jne

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 5
+main: li r0, 6
     li r1, 5
-    jeq r1, r0, 10
+    jne r1, r0, 10
         
