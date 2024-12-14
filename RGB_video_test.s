17e3ceab2ce24a392d98070efdfdaf4281367ae8
Tim Thuma <thumatim@gmail.com>
2024-12-14 15:45:43 +0100

    rgb test working

--- RGB_video_test.s
+++ RGB_video_test.s
@@ -3,10 +3,10 @@
 # and stores -1 in memory location 16.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 131072 # color, start with white + 1
+main: li r0, 131072 # color, start with white
     li r1, 49151 # address, start at top left - 1
 
-loop: dec r0
+loop: subi r0, r0, 8
 	inc r1
 	swi r0, r1, 0
 	jnez r0, loop
