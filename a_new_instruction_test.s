30daced4432722d11fb70d8e8e47defeca81fef4
Tim Thuma <thumatim@gmail.com>
2024-12-14 13:43:53 +0100

    added inc, dec

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,10 +3,5 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r7, 20
-    li r8, 10
-    jsr 10
-    li r1, 10
-    li r1, 11
-    li r1, 12
-    rts
+main: li r0, 20
+    dec r0
