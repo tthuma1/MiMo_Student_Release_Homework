c37b0c6fa3576cd9b3d8ffa5ebab794ced670d36
Tim Thuma <thumatim@gmail.com>
2024-12-14 01:15:31 +0100

    added jsr, rts

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,6 +3,10 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r1, 10
-    li r2, 10
-    bne r1, r2, 10
+main: li r7, 20
+    li r8, 10
+    jsr 10
+    li r1, 10
+    li r1, 11
+    li r1, 12
+    rts
