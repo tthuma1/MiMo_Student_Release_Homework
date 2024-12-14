0caa71cfb65900dd547e2943e14e5c6bf01d7b90
Tim Thuma <thumatim@gmail.com>
2024-12-14 15:13:26 +0100

    kinda rgb test

--- RGB_video_test.s
+++ RGB_video_test.s
@@ -3,12 +3,12 @@
 # and stores -1 in memory location 16.
 # (c) GPL3 Warren Toomey, 2012
 #
-main:	li	r2, 131071
-	sw	r2, 49152			# Save the r2	
-	sub r2, r2, 1
-	sw	r2, 49153			# Save the r2	
-	sub r2, r2, 1
-	sw	r2, 49154			# Save the r2	
-	sub r2, r2, 1
-	sw	r2, 49155			# Save the r2	
-	sub r2, r2, 1
+main: li r0, 131072 # color, start with white + 1
+    li r1, 49151 # address, start at top left - 1
+
+loop: dec r0
+	inc r1
+	swi r0, r1, 0
+	jnez r0, loop
+
+end: jmp end
