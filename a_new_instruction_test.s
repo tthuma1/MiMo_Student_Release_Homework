8fad0fdeba6c4934325881570289a3a0a9c5a2df
Tim Thuma <thumatim@gmail.com>
2024-12-14 14:17:35 +0100

    added swri

--- a_new_instruction_test.s
+++ a_new_instruction_test.s
@@ -3,7 +3,7 @@
 # down to 1 and stores the result in memory location 256.
 # (c) GPL3 Warren Toomey, 2012
 #
-main: li r0, 5
-    li r1, 10
-    sw r0, 15
-    lwri r2, r0, r1
+main: li r0, 11
+    li r1, 9
+    li r2, 15
+    swri r2, r0, r1
